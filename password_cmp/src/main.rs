025f631279a83bfa6e9e70fc90a621d250078d60
ftuernal <ftuernal@iliad-free.fr>
2025-04-29 16:59:57 +0200

    test incoming

--- password_cmp/src/main.rs
+++ password_cmp/src/main.rs
@@ -1,13 +1,8 @@
 // The intent of this code is to compare two passwords and check if they're the same
 // without using any hashing or encryption. This is a simple comparison function.
 // The code is written in Rust and uses the standard library for input/output operations.
-// It intend to be resistant to timing attacks by ensuring that the comparison takes the same amount of time regardless of the input.
-// to zeroize the memory few options are possible
-    // 1. use the `zeroize` crate
-    // 2. use `memset` from libc
-    // 3. use `std::ptr::write_bytes`
-
-// this include using unsafe code since to zero the memory we need to use `unsafe` block
+// It intends to be resistant to timing attacks by ensuring that the comparison takes 
+// the same amount of time regardless of the input.
 
 use std::env;
 use zeroize::Zeroize;
@@ -21,10 +16,16 @@ pub fn constant_pwd_cmp(a: &[u8], b: &[u8]) -> bool {
     let len = a.len();
     let mut result: u8 = 0;
 
+    //the unsafe block here is necessary to access the raw pointers of the byte slices
     unsafe {
         let a_ptr = a.as_ptr();
         let b_ptr = b.as_ptr();
         for i in 0.. len {
+            // XOR the bytes and accumulate the result
+            // this is a constant time operation 
+            // because it does not depend on the values of the bytes
+            // the result will be 0 if all bytes are equal
+            // and non-zero if they are not
             result |= *a_ptr.add(i) ^ *b_ptr.add(i);
         }
     }
@@ -36,20 +37,31 @@ fn zeroize_memory(pwd: &mut [u8]) {
     pwd.zeroize();
 }
 
-// fn zeroize_memory_v2(password: &str) {
-//     // zero the memory
-//     unsafe {
-//         let password_ptr = password.as_ptr();
-//         let password_len = password.len();
-//         std::ptr::write_bytes(password_ptr as *mut u8, 0, password_len);
-//     }
-// }
+// vulnerable impl for testing
 
-// fn zeroize_memory_v3(pwd: &mut str) {
-//     unsafe {
-//         libc::memset(pwd.as_ptr() as *mut libc::c_void, 0, pwd.len());
-//     }
-// }
+fn vulnerable_pwd_cmp(a: &[u8], b: &[u8]) -> bool {
+    if a.len() != b.len() {
+        return false;
+    }
+
+    let len = a.len();
+    let mut result = true;
+
+    // the unsafe block here is necessary to access the raw pointers of the byte slices
+    unsafe {
+        let a_ptr = a.as_ptr();
+        let b_ptr = b.as_ptr();
+        for i in 0.. len {
+            // this is not a constant time operation
+            // because it depends on the values of the bytes
+            // if the bytes are equal, the loop will continue
+            // if they are not, the loop will break
+            result &= *a_ptr.add(i) == *b_ptr.add(i);
+        }
+    }
+
+    result
+}
 
 fn main() {
 
@@ -58,6 +70,11 @@ fn main() {
         eprintln!("Usage : password_cmp <password> <password>");
         std::process::exit(1);
     }
+    // get the passwords from the command line arguments
+    // and convert them to byte arrays
+    // this is necessary because the constant time comparison function
+    // works with byte arrays
+    // and not with strings
     let mut password1 = args[1].clone().into_bytes();
     let mut password2 = args[2].clone().into_bytes();
 
@@ -67,6 +84,77 @@ fn main() {
         println!("Passwords do not match");
     }
     // zero the memory
+    // this is necessary to prevent the passwords from being leaked
+    // in memory
+    // this is a good practice to follow
+    // when dealing with sensitive information
     zeroize_memory(&mut password1);
     zeroize_memory(&mut password2);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::thread;
+    use std::time::{Duration, Instant};
+    use rand::{thread_rng, Rng};
+    use rand::distributions::Alphanumeric;
+
+    fn random_string_gen(length: usize) -> String {
+        thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(length)
+            .map(char::from)
+            .collect()
+    }
+
+    #[test]
+    fn test_matchig_passwords() {
+        let password = b"secure_password123";
+        assert!(constant_pwd_cmp(password, password));
+    }
+
+    #[test]
+    fn test_different_content_same_pwd() {
+        let password1 = b"secure_password123";
+        let password2 = b"secuer_pas^7word123";
+        assert!(!constant_pwd_cmp(password1, password2));
+    }
+
+    #[test]
+    fn test_different_length() {
+        let password1 = b"secure_password123";
+        let password2 = b"secure_password1234";
+        assert!(!constant_pwd_cmp(password1, password2));
+    }
+
+    #[test]
+    fn test_empty_passwords() {
+        let password1 = b"";
+        let password2 = b"";
+        assert!(constant_pwd_cmp(password1, password2));
+    }
+
+    #[test]
+    fn test_timing_constitency() {
+        let pwd = random_string_gen(32);
+        let pwd_bytes = pwd.as_bytes();
+
+        let iterations = 1000;
+        let positions_to_test = [0, 5, 10, 15, 20, 25, 30, 31];
+
+        let mut timings = Vec::new();
+        for &position in &positions_to_test {
+            let mut test_password = pwd.clone();
+            let char_to_change = (test_password.as_bytes()[position] as char).to_string();
+            let replace_char = if char_to_change == "A" { "B" } else { "A" };
+            test_password.replace_range(position..position+1, replace_char);
+            let test_bytes = test_password.as_bytes();
+            
+        }
+
+
+    }
+    
+
 }
